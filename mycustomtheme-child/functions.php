b47016f805deebdc18613d8e83c35c8ef9e6548a
Nkpixels <100616784+7Naresh@users.noreply.github.com>
2023-03-03 22:01:01 +0530

    Update functions.php

--- mycustomtheme-child/functions.php
+++ mycustomtheme-child/functions.php
@@ -5,5 +5,7 @@ function my_theme_enqueue_styles() {
 		array( 'parenthandle' ),
 		wp_get_theme()->get( 'Version' ) // This only works if you have Version defined in the style header.
 	);
+	
+	//wp_enqueue_style('style', get_template_directory_uri() .'/style.css');
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
